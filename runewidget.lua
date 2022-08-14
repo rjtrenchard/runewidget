303e66d4cf7e08e135e7e1e0fa103a602dc88f13
RJ Trenchard <rjtrenchard@gmail.com>
2022-08-14 00:17:48 -0700

    name change

--- runewidget.lua
+++ runewidget.lua
@@ -9,7 +9,7 @@
 --     * Redistributions in binary form must reproduce the above copyright
 --     notice, this list of conditions and the following disclaimer in the
 --     documentation and/or other materials provided with the distribution.
---     * Neither the name of RuneHelper nor the
+--     * Neither the name of Rune Widget nor the
 --     names of its contributors may be used to endorse or promote products
 --     derived from this software without specific prior written permission.
 
@@ -25,18 +25,19 @@
 -- SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 --[[
-    Rune Helper
+    Rune Widget
 
     on-screen clickable rune enchantments.
 ]]
 
-_addon.name = "Rune Helper"
+_addon.name = "Rune Widget"
 _addon.author = "rjt"
 _addon.version = "1.0"
 _addon.commands = { "rune", "rh" }
 
 config = require('config')
 images = require('images')
+require('table')
 
 defaults = {
     orient = 'v',
@@ -49,15 +50,15 @@ defaults = {
 }
 settings = config.load(defaults)
 
-rune_enchantment = {
-    Ignis = { element = 'fire', resist = 'ice' },
-    Gelus = { element = 'ice', resist = 'wind' },
-    Flabra = { element = 'wind', resist = 'stone' },
-    Tellus = { element = 'stone', resist = 'thunder' },
-    Sulpor = { element = 'thunder', resist = 'water' },
-    Unda = { element = 'water', resist = 'fire' },
-    Lux = { element = 'light', resist = 'dark' },
-    Tenebrae = { element = 'dark', resist = 'light' }
+rune_enchantment = T {
+    ignis = { element = 'fire', resist = 'ice' },
+    gelus = { element = 'ice', resist = 'wind' },
+    flabra = { element = 'wind', resist = 'stone' },
+    tellus = { element = 'stone', resist = 'thunder' },
+    sulpor = { element = 'thunder', resist = 'water' },
+    unda = { element = 'water', resist = 'fire' },
+    lux = { element = 'light', resist = 'dark' },
+    tenebrae = { element = 'dark', resist = 'light' }
 }
 
 element_colour = {
@@ -70,3 +71,26 @@ element_colour = {
     light = 'dfe6e5',
     dark = '292929'
 }
+
+do
+    local rune_base = {
+        color = { alpha = 255 },
+        texture = { fit = false },
+        draggable = false
+    }
+    local drag_base = {
+        color = { alpha = 255 },
+        texture = { fit = false },
+        draggable = false
+    }
+    rune_image = T { ignis, gelus, flabra, tellus, sulpor, unda, lux, tenebrae }
+    rune_image.ignis = images.new(rune_base)
+    rune_image.gelus = images.new(rune_base)
+    rune_image.flabra = images.new(rune_base)
+    rune_image.tellus = images.new(rune_base)
+    rune_image.sulpor = images.new(rune_base)
+    rune_image.unda = images.new(rune_base)
+    rune_image.lux = images.new(rune_base)
+    rune_image.tenebrae = images.new(rune_base)
+    drag_image = images.new(drag_base)
+end
